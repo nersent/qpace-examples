c625b821d18a92222f8114ca52162b1bbbae8ce1
Mikołaj Palkiewicz <xnerhu@gmail.com>
2025-06-29 14:00:18 +0200

    init

--- web/index.js
+++ web/index.js
@@ -1,10 +1,10 @@
 import * as qp from "qpace/web";
-import * as pine from "qpace_example_lib_wasm";
+// import * as pine from "qpace_example_lib_wasm";
 
 window.onload = async () => {
   await qp.init();
   await qp.ta.init();
-  await pine.init();
+  // await pine.init();
 
   let { default: bars } = await import("../assets/btc.json");
   bars = bars.map((r) => qp.OhlcvBar.fromJSON(r));
@@ -13,5 +13,5 @@ window.onload = async () => {
 
   const ctx = new qp.Ctx(ohlcv, qp.Sym.BTC_USD());
   console.log(qp.ta.rsi(ctx.copy(), ctx.ohlcv.close, 14));
-  console.log(pine.script.customMa(ctx.copy(), ctx.ohlcv.close));
+  // console.log(pine.script.customMa(ctx.copy(), ctx.ohlcv.close));
 };
